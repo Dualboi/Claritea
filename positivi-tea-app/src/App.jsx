a120b1aef844b920e224d055358572ddc1511a61
dualboi <sonnybell2004@gmail.com>
2026-09-07 15:08:18 +0100

    Brand Rename: claritea-app to positivi-tea-app

--- positivi-tea-app/src/App.jsx
+++ positivi-tea-app/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-900 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-white">
-        Claritea coming soon!
+        Positivi-Tea coming soon!
       </h1>
     </div>
   )

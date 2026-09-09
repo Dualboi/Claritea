69592909f65fc0b801c1462ebf9b261a9100e553
dualboi <sonnybell2004@gmail.com>
2026-09-09 16:08:51 +0100

    adding footer.

--- positivi-tea-app/src/App.jsx
+++ positivi-tea-app/src/App.jsx
@@ -1,11 +1,16 @@
+import footer from "./components/footer";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Positivi-Tea coming soon!
-      </h1>
-    </div>
-  )
+    <>
+      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">
+          Positivi-Tea coming soon!
+        </h1>
+      </div>
+      <footer />
+    </>
+  );
 }
 
-export default App
+export default App;
